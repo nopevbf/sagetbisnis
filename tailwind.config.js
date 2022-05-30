9742b0a98a69c957500936455cd6454af07ac773
nopevbf <furimanjay@gmail.com>
2022-05-31 00:56:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-primary": "#8F00A8",
+        "custom-secondary": "#F5A710",
+        "custom-third": "#BC335E",
+        "custom-fourth": "#F16E07",
+        "custom-fifth": "#B02572",
+      },
+    },
+  },
+  plugins: [],
+};
